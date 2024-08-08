f80f7bd16295498eae16d123509dd59af7902f5c
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2024-08-08 10:26:34 +0530

    push basic ops routes

---
+++ app/(ops)/rent/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Rent() {
+  return (
+    <div>
+      rent
+    </div>
+  )
+}
---
+++ app/(ops)/sell/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Sell() {
+  return (
+    <div>
+      Sell
+      <input placeholder="product" type="text" />
+      <input placeholder="price" type="number" />
+      <input placeholder="descrption" type="text" />
+      <input placeholder="phone" type="number" />
+      <input placeholder="contactinfo-note" type="text" />
+    </div>
+  )
+}

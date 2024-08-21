a05ca1c55c59b7cfc10bc7bbb823bf0154aac5bc
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2024-08-21 11:52:55 +0530

    init order_bm_1

---
+++ app/(ops)/user/order/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { cancelItem } from "@/actions/user/orders/cancelItem";
+import Navbar from "@/components/buy/navbar";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+export default function Order() {
+
+  const [orderItems, setOrderItems] = useState()
+  const { data: session } = useSession()
+  const userId = session?.user?.userId
+
+  async function fetchOrder() {
+    const response = await axios.post("http://localhost:3000/api/user/order/getOrder", { userId })
+    console.log("response from fetchOrder:\n" + JSON.stringify(response))
+    setOrderItems(response.data.order)
+  }
+
+  useEffect(() => {
+    fetchOrder()
+  }, [])
+
+  return (
+    <div>
+      <Navbar />
+      {!orderItems ? (
+        <div>empty</div>
+      ) : (
+        orderItems.map((item) => (
+          <div key={item.id}
+            className="p-4 border-2 m-2">
+            <div>{item.productId}</div>
+            <div>{item.product.name}</div>
+            <div>{item.product.price}</div>
+            <button onClick={() => cancelItemHandler(item.id)}>cancel</button>
+          </div>
+        )))}
+    </div>
+  )
+
+  async function cancelItemHandler(itemId: any) {
+    console.log("itemId: " + itemId)
+    await cancelItem(itemId);
+  }
+
+}
